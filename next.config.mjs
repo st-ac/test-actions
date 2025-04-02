57a5393eea9067d798d0ddcd535ef609df90d72c
st-ac <stephane_acar@outlook.fr>
2025-04-02 11:16:01 +0200

    modif

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: 'out',
+    images: { unoptimized: true }, // Next.js ne supporte pas bien les images optimisées sur GitHub Pages
+    basePath: '/nom-du-repo', // Important pour que GitHub Pages gère bien les liens
+  };
 
 export default nextConfig;
+
